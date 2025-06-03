da60930648897e876952069b306f3aaa509643a3
Pairadux <austincgause@gmail.com>
2025-06-03 16:58:49 -0400

    init viper

--- cmd/root.go
+++ cmd/root.go
@@ -18,19 +18,20 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-*/// }}}
+*/ // }}}
 
 package cmd
 
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var cfgFileFlag  string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -49,47 +50,66 @@ to quickly create a Cobra application.`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute() {// {{{
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
-}
+}// }}}
 
-func init() {
+func init() {// {{{
 	cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tms.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFileFlag, "config", "", "config file (default is $HOME/.tms.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}// }}}
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+	homeDir, err := os.UserHomeDir()
+	cobra.CheckErr(err)
 
-		// Search config in home directory with name ".tms" (without extension).
-		viper.AddConfigPath(home)
+	appConfigDir := filepath.Join(homeDir, ".config", "tms")
+
+	if cfgFileFlag != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFileFlag)
+	} else {
+		if _, err := os.Stat(appConfigDir); os.IsNotExist(err) {
+			cobra.CheckErr(os.MkdirAll(appConfigDir, 0o755))
+		}
+
+		viper.AddConfigPath(appConfigDir)
+		viper.AddConfigPath(".")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".tms")
+		viper.SetConfigName("config")
 	}
+
+	// viper.SetDefault("default_workspace", "inbox")
+	viper.SetDefault("example_default", "test")
 
 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			if cfgFileFlag == "" {
+				configFilePath := filepath.Join(appConfigDir, "config.yaml")
+
+				fmt.Println("Config file not found, creating default config file...")
+				cobra.CheckErr(viper.SafeWriteConfigAs(configFilePath))
+				fmt.Printf("Created default config file at: %s\n", configFilePath)
+
+				cobra.CheckErr(viper.ReadInConfig())
+			}
+		} else {
+			cobra.CheckErr(err)
+		}
 	}
 }
