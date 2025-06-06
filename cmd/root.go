f074f4b69d2469cab926102d87702a7d75f1cdf3
Pairadux <austincgause@gmail.com>
2025-06-06 14:11:57 -0400

    Only show error when config file is corrupted or unreadable

--- cmd/root.go
+++ cmd/root.go
@@ -125,15 +125,14 @@ func initConfig() { // {{{
 		cfgFilePath = filepath.Join(cfgDir, "config.yaml")
 	}
 
-	// viper.SetDefault("default_workspace", "inbox")
-	viper.SetDefault("example_default", "test")
-
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			fmt.Fprintln(os.Stderr, "Config file is corrupted or unreadable:", err)
+		}
 	} else {
-		fmt.Fprintln(os.Stderr, "Config file doesn't exist or is corrupted.\nUse `tms init <config> [opts]` to create one!")
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 } // }}}
