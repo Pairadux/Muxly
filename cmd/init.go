23bf4a7d53619252b837bd7c5c4ea293cb35ac3a
Pairadux <austincgause@gmail.com>
2025-06-09 11:52:20 -0400

    Fix(init): actualy overwrite config file
    Viper needed to be reset because the in-memory options were being merged
    with the new ones

--- cmd/init.go
+++ cmd/init.go
@@ -3,14 +3,17 @@
 
 package cmd
 
+// IMPORTS {{{
 import (
 	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/Pairadux/tms/internal/models"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-)
+)// }}}
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
@@ -23,30 +26,40 @@ Otherwise, the current config file is overwritten.
 The flags provided are used to overwrite those values in the config file.
 Any flags that are omitted will be assigned the default values shown.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if scan_dirs, _ := cmd.Flags().GetStringArray("scan_dirs"); len(scan_dirs) > 0 {
-			viper.Set("scan_dirs", scan_dirs)
+		fresh := viper.New()
+		fresh.SetConfigFile(cfgFilePath)
+
+		if scanDirs, _ := cmd.Flags().GetStringArray("scan_dirs"); len(scanDirs) > 0 {
+			fresh.Set("scan_dirs", scanDirs)
 		}
-		if entry_dirs, _ := cmd.Flags().GetStringArray("entry_dirs"); len(entry_dirs) > 0 {
-			viper.Set("entry_dirs", entry_dirs)
+		if entryDirs, _ := cmd.Flags().GetStringArray("entry_dirs"); len(entryDirs) > 0 {
+			fresh.Set("entry_dirs", entryDirs)
 		}
+		if defaultSession, _ := cmd.Flags().GetString("default_session"); defaultSession != "" {
+			fresh.Set("fallback_session", defaultSession)
+		}
+
+		sessionLayout := models.SessionLayout{
+			Windows: []models.Window{
+				{Name: "edit", Cmd: "nvim"},
+				{Name: "term", Cmd: ""},
+			},
+		}
+		fresh.Set("session_layout", sessionLayout)
+
 
 		parent := filepath.Dir(cfgFilePath)
 		_ = os.MkdirAll(parent, 0o755)
 
-		if err := viper.WriteConfigAs(cfgFilePath); err != nil {
-			if _, ok := err.(viper.ConfigFileAlreadyExistsError); ok {
-				cobra.CheckErr(viper.WriteConfig())
-			} else {
-				fmt.Fprintln(os.Stderr, "cannot write config:", err)
-				os.Exit(1)
-			}
+		if err := fresh.WriteConfigAs(cfgFilePath); err != nil {
+			fmt.Fprintln(os.Stderr, "cannot write config:", err)
+			os.Exit(1)
 		}
-
 		fmt.Println("Wrote config to", cfgFilePath)
 	},
 }
 
-func init() {// {{{
+func init() { // {{{
 	rootCmd.AddCommand(initCmd)
 
 	// Here you will define your flags and configuration settings.
@@ -59,7 +72,7 @@ func init() {// {{{
 	// is called directly, e.g.:
 	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-	initCmd.Flags().StringArrayP("scan_dirs", "s", []string{"~/Dev", "~/.dotfiles/dot_config"}, "A list of paths that should always be scanned.\nConcat with :int for depth.")	
-	initCmd.Flags().StringArrayP("entry_dirs", "e", []string{"~/Documents", "~/Cloud"}, "A list of paths that are entries themselves.")	
-}// }}}
-
+	initCmd.Flags().StringP("default_session", "d", "Documents", "The name of the default session to fall back to.")
+	initCmd.Flags().StringArrayP("scan_dirs", "s", []string{"~/Dev", "~/.dotfiles/dot_config"}, "A list of paths that should always be scanned.\nConcat with :int for depth.")
+	initCmd.Flags().StringArrayP("entry_dirs", "e", []string{"~/Documents", "~/Cloud"}, "A list of paths that are entries themselves.")
+} // }}}
