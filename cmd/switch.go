94e9dec150dfdb0f77d13d93e06df6bcc75fef88
Pairadux <austincgause@gmail.com>
2025-06-10 10:52:25 -0400

    Add usage strings to switch subcommand

--- cmd/switch.go
+++ cmd/switch.go
@@ -3,22 +3,18 @@
 
 package cmd
 
+// IMPORTS {{{
 import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-)
+)// }}}
 
 // switchCmd represents the switch command
 var switchCmd = &cobra.Command{
 	Use:   "switch",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Switch to an active session",
+	Long: `Switch to an active session`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("switch called")
 	},
