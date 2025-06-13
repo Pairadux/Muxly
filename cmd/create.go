86a7a0e0e72c4e52eb934a7765ecb5e600ed69be
Pairadux <austincgause@gmail.com>
2025-06-13 09:26:27 -0400

    Create the createCmd skeleton

---
+++ cmd/create.go
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+// © 2025 Austin Gause <a.gause@outlook.com>
+
+package cmd
+
+// IMPORTS {{{
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+) // }}}
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a session",
+	Long: `Create a session
+
+An interactive prompt for creating a session.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("create called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
