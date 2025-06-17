cd6b61ab82f0071cd0443bdc7cd620bdf2a2a0a4
Pairadux <austincgause@gmail.com>
2025-06-17 15:33:07 -0400

    Add basic example form to create subcommand

--- cmd/create.go
+++ cmd/create.go
@@ -7,6 +7,8 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Pairadux/tms/internal/forms"
+
 	"github.com/spf13/cobra"
 ) // }}}
 
@@ -20,6 +22,16 @@ An interactive prompt for creating a session.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: implement this command
 		fmt.Println("create called")
+		var burger string
+		var name string
+		var instructions string
+		var toppings []string
+		var sauceLevel int
+		var discount bool
+
+		forms.CreateForm(&burger, &name, &instructions, &toppings, &sauceLevel, &discount).Run()
+
+		fmt.Printf("burger: %s, name %s, instructions %s, toppings %s, sauceLevel %d, discount %v\n", burger, name, instructions, toppings, sauceLevel, discount)
 	},
 }
 
